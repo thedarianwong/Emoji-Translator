cef1983c1031c2d3449feaa4a0b56ddafd29f1cc
sbshaun <sbshaun7@gmail.com>
2023-02-18 15:46:34 -0800

    fixed errors

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -5,7 +5,7 @@ import FrontPage from './components/FrontPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-	<React.StrictMode>
-		<FrontPage />
-	</React.StrictMode>
+  <React.StrictMode>
+    <FrontPage />
+  </React.StrictMode>
 );
