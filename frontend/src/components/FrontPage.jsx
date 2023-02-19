690fb990c9a26a9ac7246160d345bf745ccc2eee
sbshaun <sbshaun7@gmail.com>
2023-02-19 00:16:25 -0800

    update FrontPage.jsx and FrontPage.module.scss

--- frontend/src/components/FrontPage.jsx
+++ frontend/src/components/FrontPage.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import styles from '../styles/FrontPage.module.scss';
-import { Button, Heading, VStack, HStack, Spacer, Input, Textarea } from '@chakra-ui/react';
+import {
+  Button,
+  Heading,
+  VStack,
+  HStack,
+  Spacer,
+  Textarea,
+  Text,
+  Box,
+} from '@chakra-ui/react';
 
 const server_url = process.env.REACT_APP_SERVER_URL;
 
 const FrontPage = () => {
   const [inputText, setInputText] = useState('');
   const [outputText, setOutputText] = useState('');
+  const [isOutputCopied, setIsOutputCopied] = useState(false);
 
   const handleTranslateClick = async () => {
     const response = await fetch(`${server_url}/translate`)
@@ -18,18 +28,22 @@ const FrontPage = () => {
       });
     // Replace this with your translation logic
     setOutputText(response.message);
+    setIsOutputCopied(false);
+  };
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(outputText);
+    setIsOutputCopied(true);
   };
 
   return (
     <div className={styles.frontPage}>
-      <VStack maxW = "70%"
-          minW = "70%"
-          minH = "100px">
+      <VStack maxW="70%" minW="70%" minH="100px">
         <Heading fontSize="3.5em" fontFamily="'Noto+Sans+Bamum', sans-serif">
           Emoji Translator
         </Heading>
         <Spacer />
-        <Input
+        <Textarea
           className={styles.inputBox}
           type="text"
           name="input"
@@ -38,8 +52,38 @@ const FrontPage = () => {
           onChange={event => {
             setInputText(event.target.value);
           }}
+          minH="110px"
         />
-        <Textarea className={styles.output}>{outputText}</Textarea>
+        <Box position="relative">
+          <Textarea
+            className={styles.output}
+            placeholder="output area"
+            value={outputText}
+            onChange={event => {
+              setOutputText(event.target.value);
+            }}
+            minH="177px"
+            minW="70%"
+            maxW="70%"
+            readOnly={true}
+            onClick={handleCopyClick}
+            _hover={{ cursor: 'pointer' }}
+          />
+          <Text
+            position="absolute"
+            top={0}
+            right={0}
+            mr={2}
+            mt={2}
+            fontWeight="bold"
+            fontSize="sm"
+            color="gray.500"
+            _hover={{ cursor: 'pointer', transform: 'rotate(-5deg)' }}
+            onClick={handleCopyClick}
+          >
+            {isOutputCopied ? 'Copied' : 'Copy'}
+          </Text>
+        </Box>
       </VStack>
       <HStack spacing="25px" className={styles.buttonContainer}>
         <Button
