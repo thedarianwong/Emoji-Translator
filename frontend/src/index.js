f60d5d74f5f58cfe7d4bc3777d689e6d44d54640
sbshaun <sbshaun7@gmail.com>
2023-02-18 18:12:13 -0800

    create 1 file and update 1 file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import App from './App';
+import FrontPage from './components/FrontPage';
+import { ChakraProvider } from '@chakra-ui/react';
+// import App from './App';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <ColorModeScript />
-    <App />
+    <ChakraProvider>
+      <ColorModeScript />
+      <FrontPage />
+    </ChakraProvider>
   </StrictMode>
 );
