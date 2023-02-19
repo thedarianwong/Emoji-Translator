f89816d3ef47d625a850b299600b1efb900a8e88
yuta <yuta>
2023-02-19 04:17:28 -0800

    BackEnd Base: Final

--- backend/main.py
+++ backend/main.py
@@ -5,23 +5,54 @@ app = Flask(__name__);
 
 # Access to ChatGPT
 chatbot = Chatbot(config={
-    "email":"ohmavihingsanon@gmail.com",
-    "password":"ohmavihingsanon@gmail.com"
+    "email":"hackeremoji1@gmail.com", 
+    "password":"hackeremoji1@gmail.com"
 }) 
+chatbot.clear_conversations()
 
-prev_text = ""
+# Clear the past coversation all
+# Chatbot.clear_conversations(chatbot)
+
+# First promp to translate emojis into English
+# prev_text_first = ""
+
+# for data in chatbot.ask(
+#     "No explanation, I want you to become an emoji translator. I'm going to provide a text containing emojis, and you are going to interpret the sentence and return one short English sentence that people who can't read emojis can understand. If the content is explicit, give an inexplicit response.",):
+
+#     message = data["message"][len(prev_text_first) :]
+
+#     prev_text_first = data["message"]
 
 
-for data in chatbot.ask(
-    "I want you to act as an emoji translator, I will provide you sentence that does not contain emojis, and it is your job to provide rewrite the sentence with suited emojis, You will keep the original text, and you may replace words with emojis if necessary, you may not change anything if no emoji is needed. Do not convert everything inito emojis, only add the minimum number amount of emojis if you connsider necessary and suit the context. You will only provide the translated sentence and nothing else, no explanations.",):
-    message = data["message"][len(prev_text) :]
+# Second promp to translate English into emojis
+# prev_text_second = ""
 
-    prev_text = data["message"]
+# for data in chatbot.ask(
+#     "I'll give you a text. I want you to read English sentences and decorate with suitable emojis throughout the text in random places, but it has to make sense. Also, please keep the original sentences.",):
 
-conversation_id = chatbot.get_conversations()[0]["id"]
+#     message = data["message"][len(prev_text_second) :]
+
+#     prev_text_second = data["message"]
+
+# # First Room: Capture the id to identify which conversation user wants
+# first_ID = chatbot.get_conversations()[0]["id"]
+# print("First promp id: " + first_ID)
+
+# Second Room: Capture the id to identify which conversation user wants
+# second_ID = chatbot.get_conversations()[1]["id"] 
+# print("Second promp id: " + second_ID)
+
+# print(chatbot.get_msg_history(second_ID)); 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+
+    # Chatbot.clear_conversations(chatbot)
+    while(len(chatbot.get_conversations()) > 1):
+        chatbot.delete_conversation(chatbot.get_conversations()[0]["id"])
+
+    select = request.form.get('conversion')
+    # print("Option is: " + str(select))
 
     if request.method == 'POST':
         if request.form['submit'] == 'Transalte':
@@ -32,25 +63,57 @@ def index():
             # Declare array to store all values from chatGPT
             test_arr = ""
 
-            # test_arr.append()
-            
             # Access to ChatGPT and Get a response from
             prev_text = ""
-            for data in chatbot.ask(
-                user_input, conversation_id
-            ):
 
-                message = data["message"][len(prev_text) :]
-                test_arr += message 
-                print(message, end="", flush=True)
-                prev_text = data["message"] 
+            # Define which option user made 
+            if (select == 'insert'): 
 
-            # print("Hello!!!!" + test_arr)
+                for data in chatbot.ask(
+                    "I'll give you a text. I want you to read English sentences and decorate with suitable emojis throughout the text in random places, but it has to make sense. Also, please keep the original sentences. The text is: " +  user_input
+                ):
 
-            return render_template('index.html', test_arr = test_arr)
+                    message = data["message"][len(prev_text) :]
+                    test_arr += message 
+            
+                    prev_text = data["message"] 
+                
+                # Have to set up error msg later 
+                return render_template('index.html', test_arr = test_arr, user_input = user_input)
+
+            else: 
+
+                for data in chatbot.ask(
+                    "No explanation, I want you to become an emoji translator. I'm going to provide a text containing emojis, and you are going to interpret the sentence and return one short English sentence that people who can't read emojis can understand. If the content is explicit, give an inexplicit response. The text is: " + user_input
+                ):
+
+                    message = data["message"][len(prev_text) :]
+                    test_arr += message 
+            
+                    prev_text = data["message"] 
+            
+
+                # First promp error msg (Translate emojis into words)
+                if("I'm sorry, but that text contains explicit content" in test_arr):
+                    test_arr = "Error (Explicit Content)"
+
+                # First promp error msg (Translate emojis into words)
+                elif ("doesn't have a commonly recognized meaning in English" in test_arr):
+                    test_arr = "Error (Explicit Content)"
+                
+                
+                return render_template('index.html', test_arr = test_arr, user_input = user_input)
+        
+        elif request.form['submit'] == 'Clear':
+
+            # Chatbot.clear_conversations(chatbot)
+
+            return render_template('index.html')
 
     else: 
         return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
